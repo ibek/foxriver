36ee183712b86a27e4867ec2ee4470b8d135e50b
Ivo Bek <ibek@redhat.com>
2017-12-20 17:49:04 +0100

    analytics a vrhy

--- publish.sh
+++ publish.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf build
 mkdir build
